42437350dec941dd004893f1d52721aae7fbbd7f
Yung <yung.shen@suse.com>
2021-09-03 20:36:47 +0800

    modified:   build-local-cluster.sh

--- build-local-cluster.sh
+++ build-local-cluster.sh
@@ -1,19 +1,43 @@
 #!/usr/bin/env bash
+set -e
 
-# Begin create reate 1 + 3 standard cluster
+test -f $HOME/.ssh/id_rsa.pub || ( echo "We are going to use your default id_rsa.pub, and you have it, please generate one." && exit 1 )
+
+for package in multipass cut grep tr rg k3sup
+do
+  command -v $package || exit_status="true"
+  [[ "$exit_status" = "true" ]] && echo "Please install $package"
+done
+[[ "$exit_status" = "true" ]] && echo "Please fix dependency before continue." && exit 1 || echo "Dependencies met, continue..."
+
+cloud-init.yaml() {
+cat <<-BLOCK
+#cloud-config
+users:
+- default
+- name: ubuntu
+  ssh_authorized_keys: 
+  - $(cat $HOME/.ssh/id_rsa.pub)
+BLOCK
+}
+
+
+# Begin create 1 + 3 standard cluster
 echo [INFO] Launching Instances
 
 Cluster="c1"
 ServerNameList="control"
 for Server in $ServerNameList
 do
-  multipass launch --cpus 1 --mem 2G --disk 16G --name $Cluster-$Server
+  cat <(cloud-init.yaml) | \
+    multipass launch --cpus 2 --mem 4096M --disk 16G --name $Cluster-$Server --cloud-init -
 done
 
 WorkerNameList="worker1 worker2 worker3"
 for Worker in $WorkerNameList
 do
-  multipass launch --cpus 1 --mem 2G --disk 25G --name $Cluster-$Worker
+  cat <(cloud-init.yaml) | \
+    multipass launch --cpus 1 --mem 3072M --disk 25G --name $Cluster-$Worker --cloud-init -
 done
 
 echo [INFO] Setting up k3s
@@ -22,14 +46,13 @@ sleep 3
 
 # multipass default user
 User="ubuntu"
+
 ControlNode=$(multipass list --format csv|grep control|cut -d',' -f3)
 ServerNodeList="$(multipass list --format csv|grep server|cut -d',' -f3|tr '\n' ' ')"
 WorkerNodeList="$(multipass list --format csv|grep worker|cut -d',' -f3|tr '\n' ' ')"
 
-
 echo [INFO] Setup main control node
 k3sup install --user $User --ip $ControlNode --context local-c1 --cluster
-
 
 echo [INFO] Joining additional etcd servers
 for IP in $ServerNodeList
@@ -37,7 +60,6 @@ do
   k3sup join --user $User --ip $IP --server --server-user $User --server-ip $ControlNode
   # '--kubelet-arg=root-dir=/var/lib/longhorn-test'
 done
-
 
 echo [INFO] Joining worker nodes
 # Install nodes
@@ -49,5 +71,4 @@ done
 cat << EOF
 
 export KUBECONFIG=`pwd`/kubeconfig
-
 EOF
